3f06fed5ef04b37961b01ffc4d614bf6feff5a50
Divyanshu <137319867+Divyanshu-yu@users.noreply.github.com>
2023-10-07 01:13:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here
+let regexp = /\bthe\b/\ban\b/\ba\b/gi;
+let arr=[
+	"The Virupaksha Temple","A Victoria Memorial"," An Tajmahal"
+];
+let articleLessArray=[];
+
+for(let i=0;i<arr.length;i++)
+	{
+		let articleLessItem=arr[i].replace(regexp,"").trim();
+		articleLessArray.push(articleLessItem);
+	}
+	 articleLessArray.sort();
+
+	let ul = document.querySelector('#bands');
+   for(let i=0; i<articleLessArray.length; i++){
+	let li = document.createElement("li");
+	li.textContent = ans[i];
+	ul.appendChild(li);
+   }
 
